0369d5e301133dff011735bedefd535dd520a18d
yuya hashimoto <ne25qc3b@gmail.com>
2021-09-10 02:50:48 +0900

    add: jest init + add reducer test code

---
+++ src/__test__/reducer.spec.ts
@@ -0,0 +1,18 @@
+import { paginationReducer } from '../reducer'
+
+describe('test paginationReducer', () => {
+  it('paginationReducer returns 1 when buttonType is "first"', () => {
+    const prevState = 4
+    const buttonType = "first"
+    const pageLength = 100
+    const returnValue = paginationReducer(prevState, { buttonType, pageLength })
+    expect(returnValue).toBe(1)
+  })
+  it('paginationReducer returns pageLength when buttonType is "last"', () => {
+    const prevState = 4
+    const buttonType = "last"
+    const pageLength = 100
+    const returnValue = paginationReducer(prevState, { buttonType, pageLength })
+    expect(returnValue).toBe(pageLength)
+  })
+})
