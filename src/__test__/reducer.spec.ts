42251c347d200c7818d5269ae6c413633dbb3c21
yuya hashimoto <ne25qc3b@gmail.com>
2021-09-17 11:17:26 +0900

    fix: reducer test

--- src/__test__/reducer.spec.ts
+++ src/__test__/reducer.spec.ts
@@ -5,14 +5,14 @@ describe("test paginationReducer", () => {
     const prevState = 4
     const buttonType = "first"
     const pageLength = 100
-    const returnValue = paginationReducer(prevState, { buttonType, pageLength })
+    const returnValue = paginationReducer(prevState, { type: buttonType, payload: pageLength })
     expect(returnValue).toBe(1)
   })
   it("paginationReducer returns pageLength when buttonType is \"last\"", () => {
     const prevState = 4
     const buttonType = "last"
     const pageLength = 100
-    const returnValue = paginationReducer(prevState, { buttonType, pageLength })
+    const returnValue = paginationReducer(prevState, { type: buttonType, payload: pageLength })
     expect(returnValue).toBe(pageLength)
   })
 })
